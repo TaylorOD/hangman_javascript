50b80f56fb0e4f1378685687c9d66d4650c8d7f4
taylorod <taylorsunwantedemails@gmail.com>
2020-11-05 10:14:55 -0600

    update https call

--- scripts/requests.js
+++ scripts/requests.js
@@ -1,5 +1,5 @@
 const getPuzzle = async (wordCount) => {
-  const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+  const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${wordCount}`)
 
   if (response.status === 200) {
     const data = await response.json()
